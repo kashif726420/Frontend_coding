0974e7ecfbbb13a39d2e09abe7f116587b7b4bc4
kashif726420 <mohammad.kashif@vitalanalytics.net>
2023-07-05 17:03:52 +0500

    second-commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,48 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from 'react';
 
 function App() {
+  const [attendanceData, setAttendanceData] = useState([]);
+
+  useEffect(() => {
+    fetchAttendanceData();
+  }, []);
+
+  const fetchAttendanceData = async () => {
+    try {
+      const response = await fetch('http://localhost:8000/api/attendance/1/information');
+      if (!response.ok) {
+        throw new Error('Failed to fetch attendance data');
+      }
+      const data = await response.json();
+      setAttendanceData(data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Attendance Information</h1>
+      <table>
+        <thead>
+          <tr>
+            <th>Name</th>
+            <th>Check-In</th>
+            <th>Check-Out</th>
+            <th>Total Working Hours</th>
+          </tr>
+        </thead>
+        <tbody>
+          {attendanceData.map((attendance, index) => (
+            <tr key={index}>
+              <td>{attendance.name}</td>
+              <td>{attendance.checkin || 'N/A'}</td>
+              <td>{attendance.checkout || 'N/A'}</td>
+              <td>{attendance.totalWorkingHours || 'N/A'}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
     </div>
   );
 }
